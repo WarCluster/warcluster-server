f3423080369691aa4b3d11f7fcb2176c9b588451
Kiril Vladimirov <kiril@vladimiroff.org>
2012-11-19 23:51:00 +0200

    Start playing with redis

--- main.go
+++ main.go
@@ -5,34 +5,34 @@ import (
     "github.com/garyburd/redigo/redis"
     // "./server"
     "./libs"
-    "encoding/json"
+    // "encoding/json"
 )
 
 func main() {
     db, err := redis.Dial("tcp", ":6379")
+    defer db.Close()
     if err != nil {
         log.Fatal(err)
     }
-    // server.Run()
 
     username := "gophie"
-    value1 := &libs.Player{}
-
-    sun_position := []int{499, 300}
+    sun_position := []int{500, 300}
 
     hash := libs.GenerateHash(username)
     _, home_planet := libs.GeneratePlanets(hash, sun_position)
     player := libs.CreatePlayer(username, hash, home_planet)
-    log.Println("Player:", js)
+    key, prepared_player := player.PrepareForDB()
+
+    log.Println(player)
+    log.Println(key, string(prepared_player))
 
 
-    db.Send("SET", "foo", player)
-    reply, err := redis.Values(db.Do("MGET", "foo"))
+    db.Send("SET", key, prepared_player)
+    db.Flush()
+
+    result, err := redis.String(db.Do("GET", key))
     if err != nil {
         log.Fatal(err)
     }
-    if err := redis.ScanStruct(reply, &value1); err != nil {
-        log.Fatal(err)
-    }
-    log.Print(value1)
+    log.Println(key, result)
 }
