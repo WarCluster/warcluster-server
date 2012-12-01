24a09e0496e114b3160f0f137e28e8f61711bdc9
David Tankov <lordmordevil@gmail.com>
2012-12-01 20:33:44 +0200

    Split db_manager
    Let's face it, almost everything in this module is way far from
    "connections".

--- db_manager/connection.go
+++ db_manager/connection.go
@@ -4,8 +4,6 @@ import (
     "fmt"
     "log"
     "github.com/garyburd/redigo/redis"
-    "../entities"
-    "strings"
 )
 
 var connection redis.Conn
@@ -37,54 +35,3 @@ func Finalize() {
         log.Fatal(err)
     }
 }
-
-func SetEntity(entity entities.Entity) bool {
-    key, prepared_entity := entity.Serialize()
-
-    send_err := connection.Send("SET", key, prepared_entity)
-    if send_err != nil {
-        log.Print(send_err)
-        return false
-    }
-
-    flush_err := connection.Flush()
-    if flush_err != nil {
-        log.Print(flush_err)
-        return false
-    }
-    return true
-}
-
-func GetEntity(key string) entities.Entity {
-    result, err := redis.Bytes(connection.Do("GET", key))
-    if err != nil {
-        log.Print(err)
-        return nil
-    }
-
-    return entities.Construct(key, result)
-}
-
-func GetList(group_type string, username string) []entities.Entity {
-    result, err := redis.String(connection.Do("GET", fmt.Sprintf("%v.%v", group_type, username)))
-    if err != nil {
-        log.Print(err)
-        return nil
-    }
-    var entity_list []entities.Entity
-    var coord string
-    for _, coord = range strings.Split(result, ",") {
-        key := fmt.Sprintf("%v.%v", group_type[:len(group_type)-1], coord)
-        entity_list = append(entity_list, GetEntity(key))
-    }
-    return entity_list
-}
-
-
-// func GetPlayerMissions(username string) []*entities.Mission {
-// 
-// }
-// 
-// func GetPlayerPlanets(username string) []*entities.Planet {
-// 
-// }
---
+++ db_manager/entities.go
@@ -0,0 +1,53 @@
+package db_manager
+
+import (
+    "fmt"
+    "log"
+    "github.com/garyburd/redigo/redis"
+    "../entities"
+    "strings"
+)
+
+func SetEntity(entity entities.Entity) bool {
+    key, prepared_entity := entity.Serialize()
+
+    send_err := connection.Send("SET", key, prepared_entity)
+    if send_err != nil {
+        log.Print(send_err)
+        return false
+    }
+
+    flush_err := connection.Flush()
+    if flush_err != nil {
+        log.Print(flush_err)
+        return false
+    }
+    return true
+}
+
+func GetEntity(key string) entities.Entity {
+    result, err := redis.Bytes(connection.Do("GET", key))
+    if err != nil {
+        log.Print(err)
+        return nil
+    }
+
+    return entities.Construct(key, result)
+}
+
+func GetList(group_type string, username string) []entities.Entity {
+    var entity_list []entities.Entity
+    var coord string
+    
+    result, err := redis.String(connection.Do("GET", fmt.Sprintf("%v.%v", group_type, username)))
+    if err != nil {
+        log.Print(err)
+        return nil
+    }
+
+    for _, coord = range strings.Split(result, ",") {
+        key := fmt.Sprintf("%v.%v", group_type[:len(group_type)-1], coord)
+        entity_list = append(entity_list, GetEntity(key))
+    }
+    return entity_list
+}
