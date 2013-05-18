4b5d238d8bf9ba459e63920e17e3489b37819cc4
Kiril Vladimirov <kiril@vladimiroff.org>
2013-05-19 01:55:02 +0300

    Use base path to load config files

--- config/config.go
+++ config/config.go
@@ -4,6 +4,8 @@ package config
 import (
 	"code.google.com/p/gcfg"
 	"log"
+	"path"
+	"runtime"
 )
 
 type Config struct {
@@ -18,8 +20,10 @@ type Config struct {
 	}
 }
 
-func (self *Config) Load(path string) {
-	if err := gcfg.ReadFileInto(self, path); err != nil {
+func (self *Config) Load(name string) {
+	_, filename, _, _ := runtime.Caller(1)
+	configPath := path.Join(path.Dir(filename), name)
+	if err := gcfg.ReadFileInto(self, configPath); err != nil {
 		log.Fatal("Error loading cfg:", err)
 	}
 }
