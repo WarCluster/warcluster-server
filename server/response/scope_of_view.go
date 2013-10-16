f4f9e48f8acafbdfb3578b757c3814ace7448d34
Kiril Vladimirov <kiril@vladimiroff.org>
2013-10-17 00:52:22 +0300

    Remove ScopeOfView.Entities

--- server/response/scope_of_view.go
+++ server/response/scope_of_view.go
@@ -9,7 +9,6 @@ type ScopeOfView struct {
 	Missions map[string]entities.Entity
 	Planets  map[string]entities.Entity
 	Suns     map[string]entities.Entity
-	Entities map[string]entities.Entity
 }
 
 func NewScopeOfView() *ScopeOfView {
