6695c8a521ec0e8908b691da2d746a79c51128f1
Kiril Vladimirov <kiril@vladimiroff.org>
2013-10-27 11:08:07 +0200

    Use the pool instead the database connection

--- entities/db/commands.go
+++ entities/db/commands.go
@@ -7,36 +7,36 @@ import (
 // Save takes a key (struct used as template for all data containers to ease the managing of the DB)
 // and generates an unique key in order to add the record to the DB.
 func Save(key string, value []byte) error {
-	defer mutex.Unlock()
-	mutex.Lock()
+	conn := pool.Get()
+	defer conn.Close()
 
-	_, err := connection.Do("SET", key, value)
+	_, err := conn.Do("SET", key, value)
 	return err
 }
 
 // Get is used to pull information from the DB in order to be used by the server.
 // Get operates as read only function and does not modify the data in the DB.
 func Get(key string) ([]byte, error) {
-	defer mutex.Unlock()
-	mutex.Lock()
+	conn := pool.Get()
+	defer conn.Close()
 
-	return redis.Bytes(connection.Do("GET", key))
+	return redis.Bytes(conn.Do("GET", key))
 }
 
 // GetList operates as Get, but instead of an unique key it takes a patern in order to return
 // a list of keys that reflect the entered patern.
 func GetList(pattern string) ([]interface{}, error) {
-	defer mutex.Unlock()
-	mutex.Lock()
+	conn := pool.Get()
+	defer conn.Close()
 
-	return redis.Values(connection.Do("KEYS", pattern))
+	return redis.Values(conn.Do("KEYS", pattern))
 }
 
 // I think Delete speaks for itself but still. This function is used to remove entrys from the DB.
 func Delete(key string) error {
-	defer mutex.Unlock()
-	mutex.Lock()
+	conn := pool.Get()
+	defer conn.Close()
 
-	_, err := connection.Do("DEL", key)
+	_, err := conn.Do("DEL", key)
 	return err
 }
