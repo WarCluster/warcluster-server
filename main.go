18c550cae2ed9778e4f1201be47b220561e0537b
Kiril Vladimirov <kiril@vladimiroff.org>
2013-10-27 03:16:44 +0300

    Exit safely from most of the signals

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"warcluster/config"
 	"warcluster/entities/db"
 	"warcluster/server"
+	"syscall"
 )
 
 var cfg config.Config
 
 func main() {
 	go final()
-	defer final()
 
 	cfg.Load("config/config.gcfg")
 	db.Connect(cfg.Database.Network, cfg.Database.Host, cfg.Database.Port)
@@ -20,9 +20,11 @@ func main() {
 }
 
 func final() {
-	sigtermchan := make(chan os.Signal, 1)
-	signal.Notify(sigtermchan, os.Interrupt)
-	<-sigtermchan
+	exit_chan := make(chan os.Signal, 1)
+	signal.Notify(exit_chan, syscall.SIGINT)
+	signal.Notify(exit_chan, syscall.SIGKILL)
+	signal.Notify(exit_chan, syscall.SIGTERM)
+	<-exit_chan
 
 	db.Finalize()
 	server.Stop()
