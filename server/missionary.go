abf3d9b94e10555985cc36c7b4fb94d9c7156866
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-04 17:49:06 +0200

    EndMission now returns excessShips and the missionary could use it

--- server/missionary.go
+++ server/missionary.go
@@ -12,25 +12,46 @@ import (
 // 1. When the delay ends the thread ends the mission calling EndMission
 // 2. The end of the mission is bradcasted to all clients and the mission entry is erased from the DB.
 func StartMissionary(mission *entities.Mission) {
-	target_key := fmt.Sprintf("planet.%d_%d", mission.Target[0], mission.Target[1])
+	targetKey := fmt.Sprintf("planet.%d_%d", mission.Target[0], mission.Target[1])
 	time.Sleep(time.Duration(mission.TravelTime * 1e6))
 
-	target_entity, err := entities.Get(target_key)
+	targetEntity, err := entities.Get(targetKey)
 	if err != nil {
 		log.Print("Error in target planet fetch: ", err.Error())
 		return
 	}
-	target := target_entity.(*entities.Planet)
+	target := targetEntity.(*entities.Planet)
 	target.UpdateShipCount()
 
-	result := entities.EndMission(target, mission)
-	entities.Save(result)
+	excessShips := entities.EndMission(target, mission)
+	entities.Save(target)
 
-	state_change := response.NewStateChange()
-	state_change.Planets = map[string]entities.Entity{
-		result.GetKey(): result,
+	stateChange := response.NewStateChange()
+	stateChange.Planets = map[string]entities.Entity{
+		target.GetKey(): target,
 	}
-	response.Send(state_change, sessions.Broadcast)
+	response.Send(stateChange, sessions.Broadcast)
 
 	entities.Delete(mission.GetKey())
+
+	if excessShips > 0 {
+		sourceKey := fmt.Sprintf("planet.%d_%d", mission.Source[0], mission.Source[1])
+		sourceEntity, err := entities.Get(sourceKey)
+		if err != nil {
+			log.Print("Error in target planet fetch: ", err.Error())
+			return
+		}
+
+		playerEntity, err := entities.Get(mission.Player)
+		player := playerEntity.(*entities.Player)
+
+		excessMission := player.StartMission(target, sourceEntity.(*entities.Planet), 100, "Attack")
+		excessMission.ShipCount = excessShips
+		go StartMissionary(excessMission)
+		entities.Save(excessMission)
+
+		sendMission := response.NewSendMission()
+		sendMission.Mission = excessMission
+		response.Send(sendMission, sessions.Broadcast)
+	}
 }
