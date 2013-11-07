e1d3e1ba1d746dd14b604d45ab2ea7bd27787f49
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-07 13:28:31 +0200

    Add Sun.Name and generate it in NASA-style, based on username

--- entities/utils_test.go
+++ entities/utils_test.go
@@ -8,3 +8,32 @@ func TestUsernameHash(t *testing.T) {
 		t.Error("Wrong hash length")
 	}
 }
+
+func TestExtractUsernameInitials(t *testing.T) {
+	initials := [][]string{
+		{"rob_pike", "ROB"},
+		{"K_Vladimiroff", "KVL"},
+		{"vitaliy_filipov", "VIT"},
+		{"denytodorova", "DEN"},
+		{"Mordevil", "MOR"},
+		{"Lord_Voldemort7", "LOR"},
+		{"@someecards", "SOM"},
+		{"ShitGirlsSay", "SHI"},
+		{"big_ben_clock", "BIG"},
+		{"@Oatmeal", "OAT"},
+		{"bppetrov", "BPP"},
+		{"karadimov", "KAR"},
+		{"iamdevloper", "IAM"},
+		{"_rsc", "RSC"},
+		{"golang", "GOL"},
+		{"La_Chasc0na", "LAC"},
+		{"if__fi", "IFF"},
+	}
+
+	for _, pair := range initials {
+		initial := extractUsernameInitials(pair[0])
+		if pair[1] != initial {
+			t.Errorf("extractUsernameInitials(%q) = %q wants %q", pair[0], initial, pair[1])
+		}
+	}
+}
