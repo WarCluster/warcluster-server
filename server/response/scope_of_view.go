0b69f1b9de88e3ad0218b67573aade6aef8d7556
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-27 20:26:12 +0200

    Add offset by X and Y in the canvas size

--- server/response/scope_of_view.go
+++ server/response/scope_of_view.go
@@ -8,6 +8,11 @@ import (
 	"warcluster/entities"
 )
 
+const (
+	CANVAS_OFFSET_X = 10000
+	CANVAS_OFFSET_Y = 10000
+)
+
 type ScopeOfView struct {
 	baseResponse
 	Missions map[string]entities.Entity
@@ -24,13 +29,13 @@ func NewScopeOfView() *ScopeOfView {
 // calculateCanvasSize is used to determine where is the viewable by client's area
 func calculateCanvasSize(position *vec2d.Vector, resolution []int) (*vec2d.Vector, *vec2d.Vector) {
 	topLeft := vec2d.New(
-		position.X-float64(resolution[0])/2,
-		position.Y+float64(resolution[1])/2,
+		position.X-float64(resolution[0] + CANVAS_OFFSET_X)/2,
+		position.Y+float64(resolution[1] + CANVAS_OFFSET_Y)/2,
 	)
 
 	bottomRight := vec2d.New(
-		position.X+float64(resolution[0])/2,
-		position.Y-float64(resolution[1])/2,
+		position.X+float64(resolution[0] + CANVAS_OFFSET_X)/2,
+		position.Y-float64(resolution[1] + CANVAS_OFFSET_Y)/2,
 	)
 	return topLeft, bottomRight
 }
--- server/response/scope_of_view_test.go
+++ server/response/scope_of_view_test.go
@@ -8,10 +8,10 @@ import (
 
 func TestCalculateCanvasSize(t *testing.T) {
 	topLeft, bottomRight := calculateCanvasSize(vec2d.New(20, 50), []int{800, 600})
-	if *topLeft != *vec2d.New(-380, 350) {
-		t.Errorf("topLeft is %#v, expected: %#v", *topLeft, *vec2d.New(-380, 350))
+	if *topLeft != *vec2d.New(-5380, 5350) {
+		t.Errorf("topLeft is %#v, expected: %#v", *topLeft, *vec2d.New(-5380, 5350))
 	}
-	if *bottomRight != *vec2d.New(420, -250) {
-		t.Errorf("bottomRight is %#v, expected: %#v", *bottomRight, *vec2d.New(420, -250))
+	if *bottomRight != *vec2d.New(5420, -5250) {
+		t.Errorf("bottomRight is %#v, expected: %#v", *bottomRight, *vec2d.New(5420, -5250))
 	}
 }
