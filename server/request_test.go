50e1e941fb8e26333c556a57e6fc9251fdb32303
victoria92 <viki.hristova@gmail.com>
2014-02-05 23:51:12 +0200

    Add tests for request parsing

---
+++ server/request_test.go
@@ -0,0 +1,86 @@
+package server
+
+import (
+	"testing"
+	"reflect"
+
+	"github.com/Vladimiroff/vec2d"
+)
+
+func TestAllTypesOfMission(t *testing.T) {
+
+	var tableTests = []struct {
+		input   string
+		output  func(*Request) error
+	}{
+		{"start_mission",  parseAction},
+		{"scope_of_view",  scopeOfView},
+		{"something_else", nil},
+	}
+
+	request := new(Request)
+	request.StartPlanet = "start"
+	request.EndPlanet   = "end"
+	request.Position    = vec2d.New(2.0, 4.0)
+	request.Resolution  = []uint16{1920, 1080}
+
+	for _, test := range tableTests {
+		request.Command = test.input
+		result, _ := ParseRequest(request)
+
+		result_value := reflect.ValueOf(result)
+		output_value := reflect.ValueOf(test.output)
+
+		if result_value.Pointer() != output_value.Pointer() {
+			t.Errorf("Request with %s returnes %#v, expected %#v", test.input, result, test.output)
+		}
+	}
+}
+
+func TestStartMissionWithoutEnoughArguments(t *testing.T) {
+	request := new(Request)
+	request.Command = "start_mission"
+	request.StartPlanet = "start"
+	result, _ := ParseRequest(request)
+
+	if result != nil {
+		t.Errorf("Request start_mision without EndPlanet returnes %#v", result)
+	}
+}
+
+func TestScopeOfViewWithoutEnoughArguments(t *testing.T) {
+	request := new(Request)
+	request.Command = "skope_of_view"
+	request.Position = vec2d.New(2.0, 4.0)
+	result, _ := ParseRequest(request)
+
+	if result != nil {
+		t.Errorf("Request scope_of_view without EndPlanet returnes %#v", result)
+	}
+}
+
+func TestStartMissionWithNegativeFleet(t *testing.T) {
+	request := new(Request)
+	request.Command = "start_mission"
+	request.StartPlanet = "start"
+	request.EndPlanet   = "end"
+	request.Fleet = -10
+	ParseRequest(request)
+
+	if request.Fleet != 10 {
+		t.Errorf("Request start_mision with negative fleet makes fleet size %d", request.Fleet)
+	}
+}
+
+func TestStartMissionWithMoreThanHundredFleet(t *testing.T) {
+	request := new(Request)
+	request.Command = "start_mission"
+	request.StartPlanet = "start"
+	request.EndPlanet   = "end"
+	request.Fleet = 150
+	ParseRequest(request)
+
+	if request.Fleet != 100 {
+		t.Errorf("Request start_mision with negative fleet makes fleet size %d", request.Fleet)
+	}
+}
