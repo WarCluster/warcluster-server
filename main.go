64b032ba5aab9cd1e90c1db5bd5173a1f5927bd0
Kiril Vladimirov <kiril@vladimiroff.org>
2014-02-20 00:45:53 +0200

    Introduce simple sorted slice as a leaderboard

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 
 	cfg.Load("config/config.gcfg")
 	db.InitPool(cfg.Database.Host, cfg.Database.Port, 8)
+	leaderboard.New().Init()
 	server.Start(cfg.Server.Host, cfg.Server.Port)
 }
 
