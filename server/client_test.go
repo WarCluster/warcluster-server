b9b00b4344d547dccbf42d4c94af4272d8d95736
victoria92 <viki.hristova@gmail.com>
2014-02-25 23:37:39 +0200

    Add test for login of excisting user

--- server/client_test.go
+++ server/client_test.go
@@ -8,16 +8,47 @@ import (
 	"warcluster/entities/db"
 )
 
-func TestLoginUser(t *testing.T) {
+const (
+	user = "{\"Command\": \"login\", \"Username\": \"JohnDoe\", \"TwitterId\": \"some twitter ID\"}"
+	setupParams = "{\"Command\": \"SetupParameters\", \"Fraction\": 0, \"SunTextureId\": 0}"
+)
+
+func TestLoginNewUser(t *testing.T) {
 	var session testSession
-	session.Send([]byte("{\"Command\": \"login\", \"Username\": \"JohnDoe\", \"TwitterId\": \"some twitter ID\"}"))
-	session.Send([]byte("{\"Command\": \"SetupParameters\", \"Fraction\": 0, \"SunTextureId\": 0}"))
+	session.Send([]byte(user))
+	session.Send([]byte(setupParams))
 	conn := db.Pool.Get()
 	defer conn.Close()
 
 	players_before, err := redis.Strings(conn.Do("KEYS", "player.*"))
 	before := len(players_before)
 
+	authenticate(&session)
+
+	players_after, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	after := len(players_after)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if after == before + 1 {
+		t.Fail()
+	}
+}
+
+func TestLoginExcistingUser(t *testing.T) {
+	var session testSession
+	session.Send([]byte(user))
+	session.Send([]byte(setupParams))
+	session.Send([]byte(user))
+	conn := db.Pool.Get()
+	defer conn.Close()
+
+	players_before, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	before := len(players_before)
+
+	authenticate(&session)
 	authenticate(&session)
 
 	players_after, err := redis.Strings(conn.Do("KEYS", "player.*"))
