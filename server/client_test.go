643b5687f4296165b20da9ae24b4a120a2863a2b
victoria92 <viki.hristova@gmail.com>
2014-03-11 23:02:10 +0200

    Use testify instead of testing

--- server/client_test.go
+++ server/client_test.go
@@ -3,6 +3,8 @@ package server
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"github.com/garyburd/redigo/redis"
 
 	"warcluster/entities/db"
@@ -13,127 +15,98 @@ const (
 	setupParams = "{\"Command\": \"setup_parameters\", \"Fraction\": 0, \"SunTextureId\": 0}"
 )
 
-func TestRegisterNewUser(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+type ClientTestSuite struct {
+    suite.Suite
+    conn redis.Conn
+    session *testSession
+}
 
-	session := new(testSession)
-	session.Send([]byte(user))
-	session.Send([]byte(setupParams))
+func (suite *ClientTestSuite) SetupTest() {
+    suite.conn = db.Pool.Get()
+	suite.conn.Do("FLUSHDB")
+	suite.session = new(testSession)
+}
 
-	players_before, err := redis.Strings(conn.Do("KEYS", "player.*"))
+func (suite *ClientTestSuite) TearDown() {
+	suite.conn.Close()
+}
+
+func (suite *ClientTestSuite) TestRegisterNewUser() {
+	suite.session.Send([]byte(user))
+	suite.session.Send([]byte(setupParams))
+
+	players_before, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	before := len(players_before)
 
-	if _, err := authenticate(session); err != nil {
-		t.Errorf("authenticate() failed with %s", err.Error())
-	}
+	_, err = authenticate(suite.session)
 
-	players_after, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	assert.Nil(suite.T(), err)
+
+	players_after, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	after := len(players_after)
 
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(suite.T(), err)
 
-	if after != before+1 {
-		t.Errorf("%#s\n", players_after)
-		t.Fail()
-	}
+	assert.Equal(suite.T(), before + 1, after)
 }
 
-func TestAuthenticateExcistingUser(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+func (suite *ClientTestSuite) TestAuthenticateExcistingUser() {
+	suite.session.Send([]byte(user))
+	suite.session.Send([]byte(setupParams))
+	suite.session.Send([]byte(user))
 
-	session := new(testSession)
-	session.Send([]byte(user))
-	session.Send([]byte(setupParams))
-	session.Send([]byte(user))
-
-	players_before, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	players_before, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	before := len(players_before)
 
-	authenticate(session)
-	authenticate(session)
+	authenticate(suite.session)
+	authenticate(suite.session)
 
-	players_after, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	players_after, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	after := len(players_after)
 
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(suite.T(), err)
 
-	if after != before+1 {
-		t.Fail()
-	}
+	assert.Equal(suite.T(), before + 1, after)
 }
 
-func TestAuthenticateUserWithIncompleteData(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+func (suite *ClientTestSuite) TestAuthenticateUserWithIncompleteData() {
+	suite.session.Send([]byte("{\"Command\": \"login\", \"TwitterId\": \"some twitter ID\"}"))
 
-	var session testSession
-	session.Send([]byte("{\"Command\": \"login\", \"TwitterId\": \"some twitter ID\"}"))
-
-	players_before, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	players_before, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	before := len(players_before)
 
-	authenticate(&session)
+	authenticate(suite.session)
 
-	players_after, err := redis.Strings(conn.Do("KEYS", "player.*"))
+	players_after, err := redis.Strings(suite.conn.Do("KEYS", "player.*"))
 	after := len(players_after)
 
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(suite.T(), err)
 
-	if before != after {
-		t.Fail()
-	}
+	assert.Equal(suite.T(), before, after)
 }
 
-func TestAuthenticateUserWithNilData(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+func (suite *ClientTestSuite) TestAuthenticateUserWithNilData() {
+	suite.session.Send(nil)
+	_, err := authenticate(suite.session)
 
-	session := new(testSession)
-	session.Send(nil)
-	_, err := authenticate(session)
-
-	if err == nil {
-		t.Fail()
-	}
+	assert.NotNil(suite.T(), err)
 }
 
-func TestAuthenticateUserWithInvalidJSONData(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+func (suite *ClientTestSuite) TestAuthenticateUserWithInvalidJSONData() {
+	suite.session.Send([]byte("panda"))
+	_, err := authenticate(suite.session)
 
-	session := new(testSession)
-	session.Send([]byte("panda"))
-	_, err := authenticate(session)
-
-	if err == nil {
-		t.Fail()
-	}
+	assert.NotNil(suite.T(), err)
 }
 
-func TestAuthenticateUserWithNilSetupData(t *testing.T) {
-	conn := db.Pool.Get()
-	defer conn.Close()
-	conn.Do("FLUSHDB")
+func (suite *ClientTestSuite) TestAuthenticateUserWithNilSetupData() {
+	suite.session.Send([]byte(user))
+	suite.session.Send(nil)
+	_, err := authenticate(suite.session)
 
-	session := new(testSession)
-	session.Send([]byte(user))
-	session.Send(nil)
-	_, err := authenticate(session)
+	assert.NotNil(suite.T(), err)
+}
 
-	if err == nil {
-		t.Fail()
-	}
+func TestClientTestSuite (t *testing.T) {
+    suite.Run(t, new(ClientTestSuite))
 }
