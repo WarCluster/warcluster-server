ec8a10f02238e4df798e1309692b9b9100dac96f
Kiril Vladimirov <kiril@vladimiroff.org>
2014-03-26 01:32:35 +0200

    Completely test the leaderboard

--- leaderboard/leaderboard_test.go
+++ leaderboard/leaderboard_test.go
@@ -5,20 +5,48 @@ import (
 )
 
 func initLeaderboard() *Leaderboard {
-	l := new(Leaderboard)
+	l := New()
 	l.board = []*Player{
 		{Username: "0", Planets: 10},
 		{Username: "1", Planets: 8},
 		{Username: "2", Planets: 7},
 		{Username: "3", Planets: 6},
 		{Username: "4", Planets: 5},
+		{Username: "5", Planets: 0},
+		{Username: "6", Planets: 0},
+		{Username: "7", Planets: 0},
+		{Username: "8", Planets: 0},
+		{Username: "9", Planets: 0},
+		{Username: "10", Planets: 0},
+		{Username: "11", Planets: 0},
+		{Username: "12", Planets: 0},
+		{Username: "13", Planets: 0},
+		{Username: "14", Planets: 0},
+		{Username: "15", Planets: 0},
+		{Username: "16", Planets: 0},
+		{Username: "17", Planets: 0},
+		{Username: "18", Planets: 0},
 	}
 	l.places = map[string]int{
-		"0": 0,
-		"1": 1,
-		"2": 2,
-		"3": 3,
-		"4": 4,
+		"0":  0,
+		"1":  1,
+		"2":  2,
+		"3":  3,
+		"4":  4,
+		"5":  5,
+		"6":  6,
+		"7":  7,
+		"8":  8,
+		"9":  9,
+		"10": 10,
+		"11": 11,
+		"12": 12,
+		"13": 13,
+		"14": 14,
+		"15": 15,
+		"16": 16,
+		"17": 17,
+		"18": 18,
 	}
 	return l
 }
@@ -68,4 +96,75 @@ func TestMovingDown(t *testing.T) {
 		t.Errorf("1 is not last, %s is there instead", l.board[4].Username)
 	}
 	l.moveUp(string(len(l.board) - 1))
+}
+
+func TestPlace(t *testing.T) {
+	l := initLeaderboard()
+	results := []struct {
+		in  string
+		out int
+	}{
+		{"0", 0},
+		{"4", 4},
+		{"2", 2},
+		{"42", 0},
+	}
+
+	for _, result := range results {
+		place := l.Place(result.in)
+		if place != result.out {
+			t.Errorf("Player %#v is in place %d, expected %d", result.in, place, result.out)
+		}
+	}
+}
+
+func TestPage(t *testing.T) {
+	l := initLeaderboard()
+	results := []struct {
+		in  int64
+		out int
+		err bool
+	}{
+		{1, 10, false},
+		{2, 9, false},
+		{3, 0, true},
+	}
+
+	for _, result := range results {
+		page, err := l.Page(result.in)
+		if len(page) != result.out || result.err != (err != nil) {
+			t.Errorf(
+				"Page %d returned %d (err: %t) players, expected %d (err: %t)",
+				result.in, len(page), err != nil, result.out, result.err)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	l := initLeaderboard()
+	boardLengthBefore := l.Len()
+	placesLengthBefore := len(l.places)
+	l.Add(&Player{Username: "gophie", Planets: 42})
+	if boardLengthBefore+1 != l.Len() {
+		t.Error("Board size did not changed after adding a player")
+	}
+
+	if placesLengthBefore+1 != len(l.places) {
+		t.Error("Places map size did not changed after adding a player")
+	}
+}
+
+func TestSort(t *testing.T) {
+	l := initLeaderboard()
+	l.places["0"] = 20
+	l.board[1].Planets = 128
+	l.Sort()
+
+	if l.board[0].Username != "1" {
+		t.Error("Leaderboard.Sort() did not sorted the board")
+	}
+
+	if l.places["0"] != 2 {
+		t.Error("Leaderboard.Sort() did not changed the places")
+	}
 }
