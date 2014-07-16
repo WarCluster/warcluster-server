d9aae5c8ac52555d48413f5abb8f132cf4fa025c
Kiril Vladimirov <kiril@vladimiroff.org>
2014-07-16 20:12:22 +0300

    Rename Teams to Races in ServerParams

--- server/response/server_params.go
+++ server/response/server_params.go
@@ -10,20 +10,20 @@ type ServerParams struct {
 	baseResponse
 	HomeSPM    float64 //ships per minute
 	PlanetsSPM map[string]float64
-	Teams      map[string]entities.Race
+	Races      map[string]entities.Race
 }
 
 func NewServerParams() *ServerParams {
 	var planetSizeIdx int8
 
 	r := new(ServerParams)
-	r.Teams = make(map[string]entities.Race)
+	r.Races = make(map[string]entities.Race)
 	r.PlanetsSPM = make(map[string]float64)
 
 	r.Command = "server_params"
 
 	for _, race := range entities.Races {
-		r.Teams[race.Name] = race
+		r.Races[race.Name] = race
 	}
 	r.HomeSPM = 60 / float64(entities.ShipCountTimeMod(1, true))
 	for planetSizeIdx = 1; planetSizeIdx <= 10; planetSizeIdx++ {
