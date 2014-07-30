43ed04ce011daa7d8edac3dc3fd5fac7940e9442
Kiril Vladimirov <kiril@vladimiroff.org>
2014-07-31 02:10:33 +0300

    Add benchmark tests for the logger

--- server/logger_test.go
+++ server/logger_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 
@@ -15,6 +16,66 @@ func sendToLogger(logger *Logger, message string) {
 		message:   message,
 	}
 	logger.push(record)
+}
+
+func benchmarkWithLoggerSizes(b *testing.B, masterMaxSize, loggerMaxSize int) {
+	var (
+		mockedOuput bytes.Buffer
+		record      *LogRecord
+	)
+
+	master := NewMasterLogger(masterMaxSize)
+	master.stream = &mockedOuput
+	logger := NewLogger(master, loggerMaxSize)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		record = &LogRecord{
+			timestamp: time.Now(),
+			message:   string("No, lamma, no!"),
+		}
+		logger.push(record)
+	}
+}
+
+func benchmarkWritingBack(b *testing.B, masterMaxSize, loggerMaxSize int) {
+	var mockedOuput bytes.Buffer
+
+	record := &LogRecord{
+		timestamp: time.Now(),
+		message:   string("No, lamma, no!"),
+	}
+
+	master := NewMasterLogger(masterMaxSize)
+	master.stream = &mockedOuput
+	logger := NewLogger(master, loggerMaxSize)
+
+	masterStash := make([]*LogRecord, 0, masterMaxSize)
+	for i := 0; i < masterMaxSize-loggerMaxSize; i++ {
+		record.timestamp = randomizeTime(record.timestamp)
+		masterStash = append(masterStash, record)
+		record.timestamp = time.Now()
+	}
+
+	loggerStash := make([]*LogRecord, 0, loggerMaxSize)
+	for i := 0; i < loggerMaxSize; i++ {
+		record.timestamp = randomizeTime(record.timestamp)
+		loggerStash = append(loggerStash, record)
+		record.timestamp = time.Now()
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		master.stash = masterStash[:]
+		logger.stash = loggerStash[:]
+
+		logger.writeBack()
+	}
+}
+
+func randomizeTime(timestamp time.Time) time.Time {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return timestamp.Add(time.Duration(rand.Intn(10000)) * time.Second)
 }
 
 func TestSimpleLoggerWithMaster(t *testing.T) {
@@ -63,4 +124,40 @@ func TestOverflowedMasterLogger(t *testing.T) {
 	line, err := mockedOuput.ReadBytes('\n')
 	assert.False(t, len(line) > 0)
 	assert.NotNil(t, err)
+}
+
+func BenchmarkPushingToM1024L256(b *testing.B) {
+	benchmarkWithLoggerSizes(b, 1024, 256)
+}
+
+func BenchmarkPushingToM2048L512(b *testing.B) {
+	benchmarkWithLoggerSizes(b, 1024, 256)
+}
+
+func BenchmarkPushingToM4096L1024(b *testing.B) {
+	benchmarkWithLoggerSizes(b, 1024, 256)
+}
+
+func BenchmarkPushingToM8192L2048(b *testing.B) {
+	benchmarkWithLoggerSizes(b, 1024, 256)
+}
+
+func BenchmarkPushStubbedMessage(b *testing.B) {
+	benchmarkWithLoggerSizes(b, 5000000, 5000001)
+}
+
+func BenchmarkWriteBackM1024L256(b *testing.B) {
+	benchmarkWritingBack(b, 1024, 256)
+}
+
+func BenchmarkWriteBackM2048L512(b *testing.B) {
+	benchmarkWritingBack(b, 2048, 512)
+}
+
+func BenchmarkWriteBackM4096L1024(b *testing.B) {
+	benchmarkWritingBack(b, 4096, 1024)
+}
+
+func BenchmarkWriteBackM8192L2048(b *testing.B) {
+	benchmarkWritingBack(b, 8192, 2048)
 }
